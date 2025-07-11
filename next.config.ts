21f7b9aa1ae968c11b36063839fa9cbd2696125a
sounabh <bagsounabh2003@gmail.com>
2025-07-11 18:02:05 +0530

    nextconfig2

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
-    // ✅ Ignore TypeScript errors during build
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   eslint: {
-    // ✅ Ignore ESLint errors during build
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
